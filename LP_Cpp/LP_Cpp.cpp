4fd66c6ec04543faffd438b4e7b9cbf736130325
goo <goo>
2022-12-18 00:47:41 +0200

    gui is working

--- LP_Cpp/LP_Cpp.cpp
+++ LP_Cpp/LP_Cpp.cpp
@@ -15,10 +15,10 @@ namespace fs = std::filesystem;
 
 class GUI {
 public:
-    string directory = "";
     int filesNum = 0;
     int choosenFile = NULL;
     string* arr = NULL;
+    string directory = "";
     Value jsonData;
 
 
@@ -56,15 +56,17 @@ public:
     }
     
     
-    
-    void selectFile() {
-        cout << endl << "choose a file: ";
-        //int choosenFile;
+    // the function return 0 to OK or 1 to Error
+    int selectFile() {        
+        cout << endl << "Select file: ";
         cin >> choosenFile;
-        cout << "'" + arr[choosenFile] + "'" << " is choosen." << endl;
+        if (choosenFile + 1 > filesNum) return 1;        
+        return 0;
     }
 
     void showFileContent() {
+        system("cls");
+        cout << "The file " << "'" + arr[choosenFile] + "'" << " is choosen." << endl;
         // read the data from json file
         ifstream myFile("./" + directory + "/"+ arr[choosenFile]);
         Reader reader;
@@ -73,17 +75,99 @@ public:
         reader.parse(myFile, jsonData);              
 
         // printing the whole json obj (arr of users objs)
-        cout << "\ndata from json file:" << jsonData << endl;
+        cout << "\n> file data:" << endl << jsonData << endl;
 
         //printing the amount of user objs in the arr
-        cout << "Total amount of users in this file: " << jsonData.size();
+        cout << "> Total amount of user objects in this file: " << jsonData.size() << endl;
     }
     
-    void kuni() {
-        // getting the last user obj from the json arr
-        Value lastUserObj = jsonData[jsonData.size() - 1];
+    // the function return 1 to continue or 0 to exit
+    int welcome() {
+        int select = 0;
+        
+        while (select != 1) {
+            system("cls");
+            cout << "********************************" << endl;
+            cout << "  welcome to the gui feature" << endl;
+            cout << "********************************" << endl << endl;
+
+            //cout << "Press any key to continue, press 0 to EXIT: ";
+            cout << "Select option: " << endl;
+            cout << 0 << "\t" << "EXIT" << endl;
+            cout << 1 << "\t" << "Continue" << endl;
+
+            cout << endl << "You choose: ";
+            cin >> select;
+            if (select == 0) {
+                cout << "Bye Bye" << endl;
+                return 0;
+            }
+
+        }
+        return 1;
+    }
+
+    // the function return 0 to OK or 1 to Error
+    int userObjectToOutput() {
+        int users = jsonData.size();
+        
+        int select = -2;
+        cout << "> Select user object to output:" << endl;
+        
+        if (users != 1)
+            cout << "0-" << users - 2 << "\t" << "User no.index" << endl;
+        
+        
+        cout << "'-1'" << "\t" << "Last user in the array" << endl;
+
+        cout << endl << "> You choose: ";
+        cin >> select;
+               
+        if (select == -1 || users == 1) {
+            // getting the last user obj from the json arr
+            Value lastUserObj = jsonData[jsonData.size() - 1];
+            string outputFileName = createOutputFile(lastUserObj);
+            cout << "> Succesfuly output Last User to '" + outputFileName + "' file " << endl;
+            return 0;
+        }
+
+        if (select + 2 <= users) {
+            Value userObj = jsonData[select];
+            string outputFileName = createOutputFile(userObj);
+            cout << "> Succesfuly output the no." << select << " user obj to '" + outputFileName + "' file " << endl;
+            return 0;
+        }
+        // user select the wrong number of index
+        return 1;
+        
     }
     
+    // the func return the output file name
+    string createOutputFile(Value userObj) {
+        // creating a directory that stores the json files
+        fs::create_directory("./outputs");
+
+        // added time stamp to the last user json file name
+        time_t now = time(0);
+        tm* ltm = localtime(&now);
+
+        string year = to_string(ltm->tm_year - 100);
+        string month = to_string(1 + ltm->tm_mon);
+        string day = to_string(ltm->tm_mday);
+        string hour = to_string(ltm->tm_hour);
+        string min = to_string(ltm->tm_min);
+        string sec = to_string(ltm->tm_sec);
+
+        string timeStr = day + "_" + month + "_" + year + "_" + hour + min + sec;
+
+        // create a file and writing the json user obj to it:
+        string outputFileName = "./outputs/" + timeStr + "__userFile.txt";
+        ofstream LastUserFile(outputFileName);
+
+        LastUserFile << userObj;
+        LastUserFile.close();
+        return outputFileName;
+    }
 
 
 };
@@ -92,54 +176,42 @@ int main()
 {
     cout << "LightPoint - a HomeAssignment!\n\n";
 
-    // read the data from json file
-    ifstream myFile("./myjsonsfiles/ruUsers.json");    
-    Value jsonData;
-    Reader reader;
+    GUI* gui = new GUI("myjsonsfiles");
+           
 
-    // parase the file data to json object
-    reader.parse(myFile, jsonData);
+    if (gui->welcome() == 0) return 0;
+	
+	gui->showFiles();
+    int wrongChoice = 0;
+    wrongChoice = gui->selectFile();
 
-    // printing the whole json obj (arr of users objs)
-    cout << "\ndata from json file:" << jsonData << endl;
+    while (wrongChoice) {
+        gui->showFiles();
+        wrongChoice = gui->selectFile();
+    }
+    
+    wrongChoice = 1;
+    while (wrongChoice) {
+        gui->showFileContent();
+        wrongChoice = gui->userObjectToOutput();
+    }
+	
+
+
+    
+
+
+
 
     // getting the last user obj from the json arr
-    Value lastUserObj = jsonData[jsonData.size() - 1];
+    //Value lastUserObj = jsonData[jsonData.size() - 1];
     
     // printing the last user:
-	cout << "\nLast user in the array:" << endl << lastUserObj << endl;
+	//cout << "\nLast user in the array:" << endl << lastUserObj << endl;
 
-    // creating a directory that stores the json files
-    fs::create_directory("./outputs");
-       
-    // added time stamp to the last user json file name
-    time_t now = time(0);
-    tm* ltm = localtime(&now);
-
-    string year = to_string(ltm->tm_year - 100);
-    string month = to_string(1 + ltm->tm_mon);
-    string day = to_string(ltm->tm_mday);
-    string hour = to_string(ltm->tm_hour);
-    string min = to_string(ltm->tm_min);
-    string sec = to_string(ltm->tm_sec);
-
-    string timeStr = day + "_" + month + "_" + year + "_" + hour + min + sec;
-
-    // create a file and writing the json user obj to it:
-    /*ofstream LastUserFile("./outputs/" + timeStr + "__LastUserFile.txt");   
     
-    LastUserFile << lastUserObj;
-    LastUserFile.close();*/
 
-    GUI *gui = new GUI("myjsonsfiles");
-    cout << gui->filesNum << endl;
-    /*gui->showFiles();
-    gui->selectFile();
-
-    gui->showFileContent();*/
-    /*gui.countFiles();
-    gui.storeFiles();
-    gui.showFiles();*/
+    
 
     
 
