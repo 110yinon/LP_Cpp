3613065053eacf68e38fbdf417df11d4e3527b49
goo <goo>
2022-12-18 11:43:32 +0200

    set the GUI class in seperate files

--- LP_Cpp/Gui.cpp
+++ LP_Cpp/Gui.cpp
@@ -1,1 +1,163 @@
 #include "Gui.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <json/json.h>
+#include <filesystem>
+#include <ctime>
+
+using namespace std;
+using namespace Json;
+
+namespace fs = std::filesystem;
+
+GUI::GUI(string drctr) {
+    directory = drctr;
+    countFiles();
+    storeFiles();
+}
+
+void GUI::countFiles() {
+    // directory_entry - represents a directory entry and stores addiotional file info
+    // directory_iterator - allow to iterates over the dir_entry elements
+    for (fs::directory_entry p : fs::directory_iterator(directory)) {
+        filesNum++;
+    }
+    arr = new string[filesNum];
+}
+
+void GUI::storeFiles() {
+    int index = 0;
+    for (fs::directory_entry p : fs::directory_iterator(directory)) {
+        arr[index] = p.path().filename().string();
+        index++;
+    }
+}
+
+void GUI::showFiles() {
+    system("cls");
+    cout << "your files:" << endl;
+    for (int i = 0; i < filesNum; i++)
+    {
+        cout << i << "\t" << arr[i] << endl;
+    }
+}
+
+
+// the function return 0 to OK or 1 to Error
+int GUI::selectFile() {
+    cout << endl << "Select file: ";
+    cin >> choosenFile;
+    if (choosenFile + 1 > filesNum) return 1;
+    return 0;
+}
+
+void GUI::showFileContent() {
+    system("cls");
+    cout << "The file " << "'" + arr[choosenFile] + "'" << " is choosen." << endl;
+    // read the data from json file
+    ifstream myFile("./" + directory + "/" + arr[choosenFile]);
+    Reader reader; // a class that allows to read json values
+
+    // parase the file data to json object
+    reader.parse(myFile, jsonData);
+
+    // printing the whole json obj (arr of users objs)
+    cout << "\n> file data:" << endl << jsonData << endl;
+
+    //printing the amount of user objs in the arr
+    cout << "> Total amount of user objects in this file: " << jsonData.size() << endl;
+}
+
+// the function return 1 to continue or 0 to exit
+int GUI::welcome() {
+    int select = 0;
+
+    while (select != 1) {
+        system("cls");
+        cout << "********************************" << endl;
+        cout << "  welcome to the gui feature" << endl;
+        cout << "********************************" << endl << endl;
+
+        //cout << "Press any key to continue, press 0 to EXIT: ";
+        cout << "Select option: " << endl;
+        cout << 0 << "\t" << "EXIT" << endl;
+        cout << 1 << "\t" << "Continue" << endl;
+
+        cout << endl << "You choose: ";
+        cin >> select;
+        if (select == 0) {
+            cout << "Bye Bye" << endl;
+            return 0;
+        }
+
+    }
+    return 1;
+}
+
+// the function return 0 to OK or 1 to Error
+int GUI::userObjectToOutput() {
+    int users = jsonData.size();
+
+    int select = -2;
+    cout << "> Select user object to output:" << endl;
+
+    if (users != 1)
+        cout << "0-" << users - 2 << "\t" << "User no.index" << endl;
+
+
+    cout << "'-1'" << "\t" << "Last user in the array" << endl;
+
+    cout << endl << "> You choose: ";
+    cin >> select;
+
+    if (select == -1 || users == 1) {
+        // getting the last user obj from the json arr
+        // Value - represents a json value
+        Value lastUserObj = jsonData[jsonData.size() - 1];
+        string outputFileName = createOutputFile(lastUserObj);
+        cout << "> Succesfuly output Last User to '" + outputFileName + "' file " << endl;
+        return 0;
+    }
+
+    if (select + 2 <= users) {
+        Value userObj = jsonData[select];
+        string outputFileName = createOutputFile(userObj);
+        cout << "> Succesfuly output the no." << select << " user obj to '" + outputFileName + "' file " << endl;
+        return 0;
+    }
+    // user select the wrong number of index
+    return 1;
+
+}
+
+// the func return the output file name
+string GUI::createOutputFile(Value userObj) {
+    // Value - represents a json value
+    
+    // creating a directory that stores the json files
+    fs::create_directory("./outputs");
+
+    // added time stamp to the last user json file name
+    time_t now = time(0); // current time in secs from 1/1/1970
+    tm* ltm = localtime(&now); // allow to format date & time
+
+    string year = to_string(ltm->tm_year - 100);
+    string month = to_string(1 + ltm->tm_mon);
+    string day = to_string(ltm->tm_mday);
+    string hour = to_string(ltm->tm_hour);
+    string min = to_string(ltm->tm_min);
+    string sec = to_string(ltm->tm_sec);
+
+    string timeStr = day + "_" + month + "_" + year + "_" + hour + min + sec;
+
+    // create a file and writing the json user obj to it:
+    string outputFileName = "./outputs/" + timeStr + "__userFile.txt";
+    ofstream LastUserFile(outputFileName);
+
+    LastUserFile << userObj;
+    LastUserFile.close();
+    return outputFileName;
+}
+
+
--- LP_Cpp/Gui.h
+++ LP_Cpp/Gui.h
@@ -1,6 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <json/json.h>
+
+
+
+
+using namespace std;
+using namespace Json;
+
+
 #pragma once
-class Gui
-{
+class GUI {
+public:
+    int filesNum = 0;
+    int choosenFile = NULL;
+    string* arr = NULL;
+    string directory = "";
+    Value jsonData;
+
+    // ctor
+    // gets directory to looks for json files
+    GUI(string drctr);
+
+    // count the json files in the folder
+    // updates the filesNum member
+    void countFiles();
+
+    // set the json file names in the arr above
+    // we get arr of the folder's files names 
+    void storeFiles();
+
+    // print the files to the screen
+    void showFiles();
+
+    // allow to select the file to work on
+    // the function return 0 to OK or 1 to Error
+    int selectFile();
+
+    // prints the whole json data (arr of obj) of the selected file
+    void showFileContent();
+
+    // A function that starts the first interaction with the user
+    // the function return 1 to continue or 0 to exit
+    int welcome();
+
+    // allow to select which json obj to output from the arr of objs
+    // the function return 0 to OK or 1 to Error
+    int userObjectToOutput();
+
+    // creating of the file and set the obj inside it
+    // the func return the output file name
+    string createOutputFile(Value userObj);
 
 };
+
 
